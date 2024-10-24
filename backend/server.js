dc2011ccc7e46c28ce1e16bb429cf790c1617b11
Гэрэлтбаатар Мягмар <24LP8388@Admins-iMac.local>
2024-10-24 11:58:16 +0800

    backend uusgej anh ajiluullaa

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-console.log("ajilah");
+import express, { request, response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { neon } from "@neondatabase/serverless";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8811;
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+
+const sql = neon(`${process.env.BACKEND_URL}`);
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+server.get("/", async (_, response) => {
+  const responseG = await sql`SELECT version()`;
+  const { version } = responseG[0];
+  response.json({ version });
+});
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
