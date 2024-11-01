a87868d868c2f181dfecae36ac4eeccd73d64204
gereltbaatar <mglgerelt@gmail.com>
2024-11-01 08:42:35 +0800

    reacortdiin backend hiisen

--- frontend/src/components/buttons/AddRecords.jsx
+++ frontend/src/components/buttons/AddRecords.jsx
@@ -1,15 +1,58 @@
 "use client";
 
+import { useFormik } from "formik";
 import { AddCategory, InputL } from "../parts";
 import { Close, PlusIcon } from "../svg";
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const AddRecords = () => {
   const [transactionType, setTransactionType] = useState("EXP");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const BACKEND_ENDPOINT = process.env.BACKEND_URL;
 
   const toggleTransactionType = (type) => {
     setTransactionType(type);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      amount: "",
+      description: "",
+      userId: "",
+    },
+
+    onSubmit: async (values) => {
+      const requestData = {
+        ...values,
+        transaction_type: transactionType,
+      };
+      console.log(requestData, "requestData");
+
+      const option = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(requestData),
+      };
+
+      try {
+        const response = await fetch(`${BACKEND_ENDPOINT}/records`, option);
+        const data = await response.json();
+
+        if (response.ok) {
+          router.push("/");
+        } else {
+          setErrorMessage(data.message || "Error occurred");
+        }
+      } catch (error) {
+        setErrorMessage("Network error");
+      }
+    },
+  });
 
   return (
     <div className="">
@@ -34,108 +77,127 @@ export const AddRecords = () => {
               </button>
             </form>
           </div>
-          <div className="grid grid-cols-2">
-            <div className="px-6 py-5 w-full">
-              <div className="h-full flex flex-col gap-8 justify-between">
-                <div className="h-full flex flex-col gap-5">
-                  <div className="bg-base100 h-10 rounded-[100px] grid grid-cols-2">
-                    <button
-                      className={`flex-1 py-2 font-semibold rounded-[100px] transition ${
-                        transactionType === "EXP"
-                          ? "bg-[#0166FF] text-white"
-                          : "bg-base100 text-gray-700"
-                      }`}
-                      type="button"
-                      onClick={() => toggleTransactionType("EXP")}
-                    >
-                      Expense
-                    </button>
-                    <button
-                      className={`flex-1 py-2 font-semibold rounded-[100px] transition ${
-                        transactionType === "INC"
-                          ? "bg-[#16A34A] text-white"
-                          : "bg-base100 text-gray-700"
-                      }`}
-                      type="button"
-                      onClick={() => toggleTransactionType("INC")}
-                    >
-                      Income
-                    </button>
-                  </div>
-                  <div className="h-full flex flex-col justify-between">
-                    <div className="flex flex-col gap-1">
-                      <p className="not-italic text-base font-normal font-roboto">
-                        Amount
-                      </p>
-                      <input
-                        type={"text"}
-                        className="w-full flex items-center self-stretch px-4 py-2 h-12 border border-base300 rounded-lg bg-base100 text-neutral not-italic text-base font-roboto outline-none"
-                        placeholder={"₮ 000.00"}
-                      />
+          <form action="" onSubmit={formik.handleSubmit}>
+            <div className="grid grid-cols-2">
+              <div className="px-6 py-5 w-full">
+                <div className="h-full flex flex-col gap-8 justify-between">
+                  <div className="h-full flex flex-col gap-5">
+                    <div className="bg-base100 h-10 rounded-[100px] grid grid-cols-2">
+                      <button
+                        className={`flex-1 py-2 font-semibold rounded-[100px] transition ${
+                          transactionType === "EXP"
+                            ? "bg-[#0166FF] text-white"
+                            : "bg-base100 text-gray-700"
+                        }`}
+                        type="button"
+                        onClick={() => toggleTransactionType("EXP")}
+                      >
+                        Expense
+                      </button>
+                      <button
+                        className={`flex-1 py-2 font-semibold rounded-[100px] transition ${
+                          transactionType === "INC"
+                            ? "bg-[#16A34A] text-white"
+                            : "bg-base100 text-gray-700"
+                        }`}
+                        type="button"
+                        onClick={() => toggleTransactionType("INC")}
+                      >
+                        Income
+                      </button>
                     </div>
-                    <div className="flex flex-col gap-1">
-                      <p className="not-italic text-base font-normal font-roboto">
-                        Category
-                      </p>
-                      <AddCategory />
-                    </div>
-                    <div className="grid grid-cols-2 gap-3">
+                    <div className="h-full flex flex-col justify-between">
                       <div className="flex flex-col gap-1">
                         <p className="not-italic text-base font-normal font-roboto">
-                          Date
+                          Amount
                         </p>
                         <input
-                          type={"date"}
-                          className="w-full flex items-center self-stretch justify-center px-4 py-2 h-12 border border-base300 rounded-lg bg-base100 text-neutral not-italic text-base font-roboto outline-none"
+                          id="amount"
+                          name="amount"
+                          type={"number"}
+                          value={formik.values.amount}
+                          onChange={formik.handleChange}
+                          className="w-full flex items-center self-stretch px-4 py-2 h-12 border border-base300 rounded-lg bg-base100 text-neutral not-italic text-base font-roboto outline-none"
                           placeholder={"₮ 000.00"}
                         />
                       </div>
                       <div className="flex flex-col gap-1">
                         <p className="not-italic text-base font-normal font-roboto">
-                          Time
+                          Category
                         </p>
-                        <input
-                          type={"time"}
-                          className="w-full flex items-center self-stretch justify-center px-4 py-2 h-12 border border-base300 rounded-lg bg-base100 text-neutral not-italic text-base font-roboto outline-none"
-                          placeholder={"₮ 000.00"}
-                        />
+                        {/* <AddCategory /> */}
+                      </div>
+                      <div className="grid grid-cols-2 gap-3">
+                        <div className="flex flex-col gap-1">
+                          <p className="not-italic text-base font-normal font-roboto">
+                            Date
+                          </p>
+                          <input
+                            type={"date"}
+                            className="w-full flex items-center self-stretch justify-center px-4 py-2 h-12 border border-base300 rounded-lg bg-base100 text-neutral not-italic text-base font-roboto outline-none"
+                            placeholder={"₮ 000.00"}
+                          />
+                        </div>
+                        <div className="flex flex-col gap-1">
+                          <p className="not-italic text-base font-normal font-roboto">
+                            Time
+                          </p>
+                          <input
+                            type={"time"}
+                            className="w-full flex items-center self-stretch justify-center px-4 py-2 h-12 border border-base300 rounded-lg bg-base100 text-neutral not-italic text-base font-roboto outline-none"
+                            placeholder={"₮ 000.00"}
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <button
+                    type="submit"
+                    className={`w-full py-2 text-white font-semibold rounded-[100px] ${
+                      transactionType === "EXP"
+                        ? "bg-[#0166FF]"
+                        : "bg-[#16A34A]"
+                    } transition duration-200`}
+                  >
+                    Add Record
+                  </button>
                 </div>
-                <button
-                  type="submit"
-                  className={`w-full py-2 text-white font-semibold rounded-[100px] ${
-                    transactionType === "EXP" ? "bg-[#0166FF]" : "bg-[#16A34A]"
-                  } transition duration-200`}
-                >
-                  Add Record
-                </button>
               </div>
-            </div>
-            <div className="p-6 pt-5 flex flex-col gap-8">
-              <div className="flex flex-col gap-1">
-                <p className="not-italic text-base font-normal font-roboto">
-                  Name
-                </p>
-                <InputL placeholder={"Write here"} />
-              </div>
-              <div className="">
+              <div className="p-6 pt-5 flex flex-col gap-8">
                 <div className="flex flex-col gap-1">
                   <p className="not-italic text-base font-normal font-roboto">
-                    Note
+                    Name
                   </p>
-                  <textarea
-                    placeholder="Write here"
-                    className="w-full max-h-60 h-full p-4 resize-none outline-none  border border-base300 bg-base100 rounded-lg font-roboto font-normal not-italic text-[#94A3B8]"
-                    cols="30"
-                    rows="10"
-                    required
-                  ></textarea>
+                  <InputL
+                    htmlFor={"name"}
+                    id={"name"}
+                    name={"name"}
+                    type={"text"}
+                    placeholder={"Write here"}
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+                <div className="">
+                  <div className="flex flex-col gap-1">
+                    <p className="not-italic text-base font-normal font-roboto">
+                      Note
+                    </p>
+                    <textarea
+                      id="description"
+                      name="description"
+                      placeholder="Write here"
+                      className="w-full max-h-60 h-full p-4 resize-none outline-none  border border-base300 bg-base100 rounded-lg font-roboto font-normal not-italic text-[#94A3B8]"
+                      cols="30"
+                      rows="10"
+                      value={formik.values.description}
+                      onChange={formik.handleChange} // onChange функц
+                    ></textarea>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </dialog>
     </div>
